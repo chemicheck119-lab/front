c1b82838d2aebaa20d9c414e81fe5c96b6580542
hywznn <choihj0510@naver.com>
2026-09-18 16:14:45 +0900

    feat: naturalize welcome section scrolling

--- new-ui/src/pages/WelcomePage.tsx
+++ new-ui/src/pages/WelcomePage.tsx
@@ -1,9 +1,18 @@
+import { useEffect } from "react";
 import OpenSection from "../components/welcome/OpenSection";
 import ProblemSection from "../components/welcome/ProblemSection";
 import FeatureSection from "../components/welcome/FeatureSection";
 import StartSection from "../components/welcome/StartSection";
 
 function WelcomePage() {
+  useEffect(() => {
+    document.documentElement.classList.add("welcome-scroll-root");
+
+    return () => {
+      document.documentElement.classList.remove("welcome-scroll-root");
+    };
+  }, []);
+
   return (
     <main className="welcome-page">
       <OpenSection />
