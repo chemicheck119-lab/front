20d1247f56fe16d4eeb7485bf40d3578dc4f7bb5
hywznn <choihj0510@naver.com>
2026-09-18 15:55:17 +0900

    feat: migrate root app to new ui

--- src/main.tsx
+++ src/main.tsx
@@ -1,7 +1,12 @@
 
+  import { StrictMode } from "react";
   import { createRoot } from "react-dom/client";
-  import App from "./app/App.tsx";
-  import "./styles/index.css";
+  import App from "../new-ui/src/App";
+  import "../new-ui/src/styles/global.css";
 
-  createRoot(document.getElementById("root")!).render(<App />);
+  createRoot(document.getElementById("root")!).render(
+    <StrictMode>
+      <App />
+    </StrictMode>,
+  );
   
