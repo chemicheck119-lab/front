0fc57f630a91d6249bb4fa4f44fe123dac8522cc
hywznn <choihj0510@naver.com>
2026-09-20 01:03:06 +0900

    fix: 기존 main UI 유지하고 workspace 분리

--- src/App.tsx
+++ src/App.tsx
@@ -1,6 +1,7 @@
 import { BrowserRouter, Routes, Route } from "react-router-dom";
 import WelcomePage from "./pages/WelcomePage";
 import OnboardingPage from "./pages/OnboardingPage";
+import MainPage from "./pages/MainPage";
 import RecordsPage from "./pages/RecordsPage";
 import FeaturesPage from "./pages/FeaturesPage";
 import PublicDataPage from "./pages/PublicDataPage";
@@ -14,7 +15,8 @@ function App() {
       <Routes>
         <Route path="/" element={<WelcomePage />} />
         <Route path="/onboarding" element={<OnboardingPage />} />
-        <Route path="/main" element={<AuthenticatedRoute>{(session) => <IntegratedMainPage session={session} />}</AuthenticatedRoute>} />
+        <Route path="/main" element={<MainPage />} />
+        <Route path="/workspace" element={<AuthenticatedRoute>{(session) => <IntegratedMainPage session={session} />}</AuthenticatedRoute>} />
         <Route path="/records" element={<RecordsPage />} />
         <Route path="/features" element={<FeaturesPage />} />
         <Route path="/public-data" element={<PublicDataPage />} />
